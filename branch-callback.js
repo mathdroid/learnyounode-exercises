46c1aa416892e3db32e24763e39d41ea0fb13125
https://twitter.com/mathdroid <muhammad.mustadi@gmail.com>
2020-12-18 16:16:48 +0700

    branch callback

--- branch-callback.js
+++ branch-callback.js
@@ -5,28 +5,22 @@ const text = {
 };
 
 function branch(arg, callback_1, callback_2) {
-  // kalau arg ada di key-nya variable text,
-  //    panggil callback_1 dengan argumen value dari text[arg]
-  // kalau nggak ada
-  //    panggil callback_2 dengan argumen error message
   if (text[arg]) {
-    // file bisa dibaca (anggep aja)
     callback_1(text[arg]);
   } else {
     callback_2(`${arg} tidak ditemukan`);
   }
 }
+
+function printSuccess(data) {
+  console.log("YES! data found!");
+  console.log(data);
+}
+
 function printError(errorMessage) {
   console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
   console.log(errorMessage);
   console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
 }
-branch(
-  process.argv[2],
-  function (data) {
-    console.log(data);
-    console.log(data);
-    console.log(data);
-  },
-  printError
-);
+
+branch(process.argv[2], printSuccess, printError);
